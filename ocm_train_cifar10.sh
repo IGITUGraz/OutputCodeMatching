ee950704e1ddee5662bdf88959c985fdcb6ed0fa
Ozdenizci Ozan <ozan@figipc69.tugraz.at>
2022-03-16 09:46:46 +0100

    sample run scripts from paper

--- ocm_train_cifar10.sh
+++ ocm_train_cifar10.sh
@@ -7,12 +7,10 @@ dir="data/"
 model="resnet20_quan"
 classes=10
 
-# Train vanilla and piecewise clustering regularized ResNet-20 networks (8-bit) and OCM models on CIFAR-10
-python -u main.py --data_dir $dir --dataset $data -c $classes --arch $model --bits 8 --outdir "results/cifar10/resnet20_quan8/"
-python -u main.py --data_dir $dir --dataset $data -c $classes --arch $model --bits 8 --outdir "results/cifar10/resnet20_quan8_PC/" -pc -lam 1e-3
-python -u main.py --data_dir $dir --dataset $data -c $classes --arch $model --bits 8 --ocm --code_length 16 --output_act "tanh" -wd 1e-4 --outdir "results/cifar10/resnet20_quan8_OCM16/"
-python -u main.py --data_dir $dir --dataset $data -c $classes --arch $model --bits 8 --ocm --code_length 32 --output_act "tanh" -wd 1e-4 --outdir "results/cifar10/resnet20_quan8_OCM32/"
-python -u main.py --data_dir $dir --dataset $data -c $classes --arch $model --bits 8 --ocm --code_length 64 --output_act "tanh" -wd 1e-4 --outdir "results/cifar10/resnet20_quan8_OCM64/"
+# Train ResNet-20 models (8-bit) with output code matching on CIFAR-10
+python -u main.py --data_dir $dir --dataset $data -c $classes --arch $model --bits 8 --ocm --code_length 16 --output_act "tanh" --outdir "results/cifar10/resnet20_quan8_OCM16/"
+python -u main.py --data_dir $dir --dataset $data -c $classes --arch $model --bits 8 --ocm --code_length 32 --output_act "tanh" --outdir "results/cifar10/resnet20_quan8_OCM32/"
+python -u main.py --data_dir $dir --dataset $data -c $classes --arch $model --bits 8 --ocm --code_length 64 --output_act "tanh" --outdir "results/cifar10/resnet20_quan8_OCM64/"
 
 # Evaluate Stealthy TA-LBF and Stealthy T-BFA attacks on OCM defended models
 python -u attack_talbf.py --data_dir $dir --dataset $data -c $classes --arch $model --bits 8 --ocm --code_length 64 --output_act "tanh" --outdir "results/cifar10/resnet20_quan8_OCM64/"
--- ocm_train_imagenet.sh
+++ ocm_train_imagenet.sh
@@ -8,9 +8,9 @@ model="resnet50_quan"
 classes=1000
 
 # Train vanilla ResNet-50 models (8-bit) on ImageNet and perform finetuning to obtain OCM models
-python -u main.py --data_dir $dir --dataset $data -c $classes --arch $model --bits 8 -e 100 -b 256 -wd 1e-4 --outdir "results/imagenet/resnet50_quan8/"
+python -u main.py --data_dir $dir --dataset $data -c $classes --arch $model --bits 8 -e 100 -b 256 --outdir "results/imagenet/resnet50_quan8/"
 python -u main.py --data_dir $dir --dataset $data -c $classes --arch $model --bits 8 --ocm --code_length 1024 --output_act "tanh" --finetune --ft_path "results/imagenet/resnet50_quan8/" -e 60 -b 256 --opt "adam" --lr 1e-3 -wd 0.0 --outdir "results/imagenet/resnet50_quan8_OCM1024/"
 
-# Evaluate Stealthy T-BFA attacks on vanilla ResNet-50 and OCM defended models
+# Evaluate Stealthy T-BFA attacks on vanilla ResNet-50 and then OCM defended models
 python -u attack_tbfa.py --data_dir $dir --dataset $data -c $classes --arch $model --bits 8 --outdir "results/imagenet/resnet50_quan8/"
 python -u attack_tbfa.py --data_dir $dir --dataset $data -c $classes --arch $model --bits 8 --ocm --code_length 1024 --output_act "tanh" --outdir "results/imagenet/resnet50_quan8_OCM1024/"
